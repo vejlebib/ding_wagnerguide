8e0885b90ffd1901435309f9d8083a3245a468b2
wiredloose <ea@vejlebib.dk>
2014-02-04 10:31:06 +0100

    updating javascript to match markup/DOM in ding2/DDB CMS

--- js/ding_wagnerguide.js
+++ js/ding_wagnerguide.js
@@ -8,69 +8,71 @@
 // Container object for all our availability stuff.
 Drupal.dingWagnerguide = {};
 
-$(document).ready(function() {
-  // Check if there is a container for ting-availability.
-  if ($("#ting-object .ting-availability").length > 0) {
+(function($){
+  Drupal.behaviors.dingWagnerguide = {
+    attach: function(context, settings) {
+      // Check if there is a container for ting-availability.
+      if ($(".pane-ting-object .holdings").length > 0) {
 
-    // Find item ID.
-    if (itemContainer = $('div.ting-item').filter(function () {
-      return this.id.match(/ting-item-\d+/);
-    }).get(0)) {
-      Drupal.dingWagnerguide.itemId = itemContainer.id.match(/\d+/);
+        // Find item ID.
+        if (itemContainer = $('div.holdings').filter(function () {
+            return $(this).attr('class').match(/holdings-\d+/);
+            }).get(0)) {
+          Drupal.dingWagnerguide.itemId = $(itemContainer).attr('class').match(/holdings-(\d+)/)[1];
 
-      // Get WagnerGUIDE links.
-      if (Drupal.settings.trampolinePath) {
-        var ajax_path = Drupal.settings.trampolinePath;
-      } else {
-        var ajax_path = Drupal.settings.basePath;
+          // Get WagnerGUIDE links.
+          ajax_path = Drupal.settings.basePath;
+          $.getJSON(ajax_path + 'ding/wagnerguide/item/' + Drupal.dingWagnerguide.itemId, {}, function(data) {
+            Drupal.dingWagnerguide.data = data;
+            alert('lines: '+$('.availability-holdings-table tbody tr').length);
+            // ...wait for the ting-availability module to write the holdings in the container.
+            var waitForContentTimer = window.setInterval(function() {
+              if ($('.availability-holdings-table tbody tr').length) {
+                clearInterval(waitForContentTimer);
+                Drupal.dingWagnerguide.populate();
+              }
+            }, 1000);
+          });
+        }
       }
 
-      // Hardcode the path until trampoline is working.
-      ajax_path = Drupal.settings.basePath;
-
-      // Get the links, and then...
-      $.getJSON(ajax_path + 'ding/wagnerguide/item/' + Drupal.dingWagnerguide.itemId, {}, function(data) {
-        Drupal.dingWagnerguide.data = data;
-        // ...wait for the ting-availability module to write the holdings in the container.
-        var waitForContentTimer = window.setInterval(function() {
-          if ($('.ting-availability p').length) {
-            clearInterval(waitForContentTimer);
-            Drupal.dingWagnerguide.populate();
-          }
-        }, 1000);
-      });
     }
   }
-});
+})(jQuery);
 
 
 Drupal.dingWagnerguide.populate = function() {
   var wagnerLinks = Drupal.dingWagnerguide.data[Drupal.dingWagnerguide.itemId];
-  //var buttonImg = '<img src="/' + Drupal.settings.ding_wagnerguide.modulePath + '/images/button-wagner.png"/>';
-	
+
   // Run through the printed holdings and insert links/popups.
   //   Hope that the lines are in the same order as our links as we have no id on the individual lines.
-  $('.ting-availability ul.library-list li').each(function(index){
-    if (wagnerLinks[index]) {
+  (function($){
 
-      if (wagnerLinks[index].popup) {
-        var link = $(this).prepend(' <a class="wagnerguide popup" title="Click the button for more information"><span>Find</span></a> ');
-        var $dialog = $('<div></div>').html(wagnerLinks[index].popup).dialog({
-          autoOpen: false,
-          title: ''
-        });
-        link.click(function() {
-          $dialog.dialog('open');
-          return false;
-        });
+    $('.availability-holdings-table tbody tr').each(function(index){
+      if (wagnerLinks[index]) {
 
-      } else if (wagnerLinks[index].href) {
-        $(this).prepend('<a class="wagnerguide map" href="' + wagnerLinks[index].href + '" target="_blank" title="Click and see on a map where the material is located in the library"><span>Find</span></a> ');
+        if (wagnerLinks[index].popup) {
+          var link = $(this).find('td:first').prepend(' <a class="wagnerguide popup" title="Click the button for more information"><span>Find</span></a> ');
+          var $dialog = $('<div></div>').html(wagnerLinks[index].popup).dialog({
+            autoOpen: false,
+            title: ''
+          });
+          link.click(function() {
+            $dialog.dialog('open');
+            return false;
+          });
+
+        } else if (wagnerLinks[index].href) {
+          $(this).find('td:first').prepend('<a class="wagnerguide map" href="' + wagnerLinks[index].href + '" target="_blank" title="Click and see on a map where the material is located in the library"><span>Find</span></a> ');
+        }
+
+        if (wagnerLinks[index].debug) {
+          $(this).find('td:first').append('<!-- ' + wagnerLinks[index].debug + ' -->');
+        }
       }
+    });
 
-      if (wagnerLinks[index].debug) {
-        $(this).append('<!-- ' + wagnerLinks[index].debug + ' -->');
-      }
-    }
-  });
+  })(jQuery);
+
 }
+
